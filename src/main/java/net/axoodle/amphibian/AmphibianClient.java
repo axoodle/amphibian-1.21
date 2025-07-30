c1d7bcd281ce634ae85a7cd40b3b796f4415e25d
axoodle <bitr8@outlook.com>
2025-07-30 16:01:40 -0700

    initial commit

---
+++ src/main/java/net/axoodle/amphibian/AmphibianClient.java
@@ -0,0 +1,10 @@
+package net.axoodle.amphibian;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AmphibianClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
